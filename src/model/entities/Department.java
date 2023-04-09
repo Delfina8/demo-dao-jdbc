235bdac9f85a8b22ea8b14c69100f43010aa3adf
Delfina8 <githubdelfina@gmail.com>
2023-04-09 18:28:19 -0300

    model department

---
+++ src/model/entities/Department.java
@@ -0,0 +1,66 @@
+package model.entities;
+
+public class Department {
+	
+	private Integer id;
+	private String name;
+	
+	public Department(Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public Department () {
+		
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	/*O hashCode e o equals servem para que o objeto seja comparado pelo seu conteúdo e não pela
+	 * referência de ponteiros
+	 * (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+	
+	
+
+}
